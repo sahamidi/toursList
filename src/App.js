9cc5023762426dcecdeb3baef751965a58b7fcaa
Sal Hamidi <hamidisa87@gmail.com>
2021-02-19 15:59:01 -0500

    Tour list completed

--- src/App.js
+++ src/App.js
@@ -4,8 +4,55 @@ import Tours from './Tours'
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
+
 function App() {
-  return <h2>Tours Project Setup</h2>
+  let [loading, setLoading] = useState(true)
+  let [tours, setTours] = useState([])
+
+  let removeTour = (id) => {
+    let newTours = tours.filter((tour) => tour.id !== id)
+    setTours(newTours)
+  }
+
+  let fetchTours = async () => {
+    setLoading(true)
+
+    try {
+      let response = await fetch(url)
+      let tours = await response.json()
+      setLoading(false)
+      setTours(tours)
+    } catch (error) {
+      setLoading(false)
+      console.log(error)
+    }
+  }
+  useEffect(() => {
+    fetchTours()
+  }, [])
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    )
+  }
+  if (tours.length === 0) {
+    return (
+      <div className='title'>
+        <h2>no tours left</h2>
+        <button className='btn' onClick={fetchTours}>
+          refresh
+        </button>
+      </div>
+    )
+  }
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  )
 }
 
 export default App
